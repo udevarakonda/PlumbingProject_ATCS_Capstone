cedf205ca9600b7246515fdcec1100ecbd935054
udevarakonda <udbhav.devarakonda@gmail.com>
2023-12-04 02:33:18 -0500

    Added Avalonia Application, HelloWorld Executable

---
+++ HelloWorld/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace HelloWorld.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ HelloWorld/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HelloWorld.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
