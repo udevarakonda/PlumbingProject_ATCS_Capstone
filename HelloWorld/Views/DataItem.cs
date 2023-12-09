ae6aa80e530442089ad2684453f0682636adc479
udevarakonda <udbhav.devarakonda@gmail.com>
2023-12-09 14:33:38 -0500

    Added customizable writes and displaying table

---
+++ HelloWorld/Views/DataItem.cs
@@ -0,0 +1,6 @@
+// DataItem.cs
+public class DataItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- HelloWorld/Views/MainWindow.axaml.cs
+++ HelloWorld/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 // MainWindow.axaml.cs
 
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
@@ -10,19 +11,28 @@ namespace HelloWorld.Views
 {
     public partial class MainWindow : Window
     {
-        // private TextBlock message;
+        private TextBox keyTextBox;
+        private TextBox nameTextBox;
+        private TextBlock messageTextBlock;
 
         public MainWindow()
         {
             InitializeComponent();
-            message = this.FindControl<TextBlock>("message");
+            messageTextBlock = this.FindControl<TextBlock>("message");
+            keyTextBox = this.FindControl<TextBox>("keyBox");
+            nameTextBox = this.FindControl<TextBox>("nameBox");
         }
 
-        public void ClickHandler(object sender, RoutedEventArgs args)
-        {
-            var button = (Button)sender;
+        // private void InitializeComponent()
+        // {
+        //     AvaloniaXamlLoader.Load(this);
 
-            // Modify the connection string with your MySQL server details
+        //     // Initialize controls
+            
+        // }
+
+        public void WriteButtonClick(object sender, RoutedEventArgs args)
+        {
             string connectionString = "Server=sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com;Database=capstone_2324_pallet;User ID=pallet;Password=8KFj9WnbfRDS;";
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
@@ -31,36 +41,111 @@ namespace HelloWorld.Views
                 {
                     connection.Open();
 
-                    if (button.Content.ToString() == "Read")
-                    {
-                        string query = "SELECT name FROM PlumbingVariables;";
-                        MySqlCommand command = new MySqlCommand(query, connection);
+                    // Get values from TextBoxes
+                    int id = Convert.ToInt32(keyTextBox.Text);
+                    string name = nameTextBox.Text;
 
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                string result = reader.GetString(reader.GetOrdinal("name"));
-                                message.Text = result;
-                            }
-                        }
-                    }
-                    else if (button.Content.ToString() == "Write")
-                    {
-                        string nameToWrite = inputTextBox.Text;
-                        string query = $"INSERT INTO PlumbingVariables (name) VALUES ('{nameToWrite}');";
-                        MySqlCommand command = new MySqlCommand(query, connection);
+                    // Insert values into 'testtable'
+                    string insertQuery = $"INSERT INTO testtable (id, name) VALUES ({id}, '{name}');";
+                    MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection);
+                    insertCommand.ExecuteNonQuery();
 
-                        command.ExecuteNonQuery();
-                        message.Text = "Data written to the PlumbingVariables table!";
-                    }
+                    messageTextBlock.Text = $"Data written to testtable: {id}: {name}";
                 }
                 catch (Exception ex)
                 {
-                    // Handle exceptions (e.g., display an error message)
-                    message.Text = $"Error: {ex.Message}";
+                    messageTextBlock.Text = $"Error: {ex.Message}";
                 }
             }
         }
+
+        public void ReadButtonClick(object sender, RoutedEventArgs args)
+        {
+            string connectionString = "Server=sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com;Database=capstone_2324_pallet;User ID=pallet;Password=8KFj9WnbfRDS;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Retrieve values from 'testtable'
+                    string readQuery = "SELECT * FROM testtable;";
+                    MySqlCommand readCommand = new MySqlCommand(readQuery, connection);
+
+                    using (MySqlDataReader reader = readCommand.ExecuteReader())
+                    {
+                        // Clear existing data
+                        dataStackPanel.Children.Clear();
+
+                        while (reader.Read())
+                        {
+                            // Create a TextBlock to display each row of data
+                            TextBlock rowTextBlock = new TextBlock
+                            {
+                                Text = $"{reader["id"]}: {reader["name"]}",
+                                Margin = new Thickness(0, 5, 0, 0)
+                            };
+
+                            // Add the TextBlock to the StackPanel
+                            dataStackPanel.Children.Add(rowTextBlock);
+                        }
+                    }
+
+                    messageTextBlock.Text = "Data loaded from testtable.";
+                }
+                catch (Exception ex)
+                {
+                    messageTextBlock.Text = $"Error: {ex.Message}";
+                }
+            }
+        }
+
+
+
+        // public void ClickHandler(object sender, RoutedEventArgs args)
+        // {
+        //     var button = (Button)sender;
+
+        //     // Modify the connection string with your MySQL server details
+        //     string connectionString = "Server=sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com;Database=capstone_2324_pallet;User ID=pallet;Password=8KFj9WnbfRDS;";
+
+        //     using (MySqlConnection connection = new MySqlConnection(connectionString))
+        //     {
+        //         try
+        //         {
+        //             connection.Open();
+
+        //             if (button.Content.ToString() == "Read")
+        //             {
+        //                 string query = "SELECT name FROM PlumbingVariables;";
+        //                 MySqlCommand command = new MySqlCommand(query, connection);
+
+        //                 using (MySqlDataReader reader = command.ExecuteReader())
+        //                 {
+        //                     while (reader.Read())
+        //                     {
+        //                         string result = reader.GetString(reader.GetOrdinal("name"));
+        //                         message.Text = result;
+        //                     }
+        //                 }
+        //             }
+        //             else if (button.Content.ToString() == "Write")
+        //             {
+        //                 string nameToWrite = inputTextBox.Text;
+        //                 string query = $"INSERT INTO PlumbingVariables (name) VALUES ('{nameToWrite}');";
+        //                 MySqlCommand command = new MySqlCommand(query, connection);
+
+        //                 command.ExecuteNonQuery();
+        //                 message.Text = "Data written to the PlumbingVariables table!";
+        //             }
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             // Handle exceptions (e.g., display an error message)
+        //             message.Text = $"Error: {ex.Message}";
+        //         }
+        //     }
+        // }
     }
 }
